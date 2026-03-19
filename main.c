4342ee870a5033da5d896e7ee37cc15dc0485643
Tommy-Sanjaya123 <tomssanz413415083@gmail.com>
2026-03-20 00:08:44 +0800

    create main program

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "student.h"
+
+int main() {
+    Student students[MAX_STUDENTS];
+    int count = 0;
+
+    printf("Student Grade Management System\n");
+
+    return 0;
+}
