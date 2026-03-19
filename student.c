b6f3ee073d6da2b388b3610f39b5455b1fc558e2
Tommy-Sanjaya123 <tomssanz413415083@gmail.com>
2026-03-20 00:12:01 +0800

    add student input function

--- student.c
+++ student.c
@@ -2,5 +2,15 @@
 #include "student.h"
 
 void addStudent(Student students[], int *count) {
-    printf("Add student function is ready.\n");
+    printf("Enter student name: ");
+    scanf(" %[^\n]", students[*count].name);
+
+    printf("Enter student ID: ");
+    scanf("%s", students[*count].id);
+
+    printf("Enter student score: ");
+    scanf("%f", &students[*count].score);
+
+    (*count)++;
+    printf("Student added successfully.\n");
 }
